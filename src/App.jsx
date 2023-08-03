b478ed3f4f3e7e640341902055133c6019b8bcd7
davadaviar <davadaviarsaputra2345@gmail.com>
2023-08-03 14:46:45 +0700

    Fix mapping

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import AccountItem from "./components/AccountItem"
+import { accountItems } from "./constants";
 
 
 function App() {
 
   return (
-    <div className='tweet-container px-3 py-3 mx-5 md:max-w-3/4 md:mx-auto lg:max-w-1/2'>
-        <AccountItem />
+    <div className='tweet-container flex-col py-8 px-5 md:mx-auto md:max-w-3/4 lg:max-w-1/2'>
+      {
+        accountItems.map((accItem) => (
+          <AccountItem
+            name={accItem.name}
+            username={accItem.username}
+            imageProfile={accItem.imageProfile}
+            createAt={accItem.createAt}
+            tweet={accItem.tweet}
+            engagement={accItem.engagements}
+          />
+        ))
+      }
     </div>
   )
 }
--- src/components/AccItemCard.jsx
+++ src/components/AccItemCard.jsx
@@ -2,30 +2,52 @@ import React from 'react'
 import EngagementItem from './EngagementItem'
 import { mardiguWowiek } from '../assets/images'
 import { bookMark, like, reply, retweet } from '../assets/icons'
+import Tilt from 'react-parallax-tilt'
+import { accountItems } from '../constants'
 
-const AccItemCard = () => {
+const AccItemCard = ({ name, username, imageProfile, createAt, tweet, engagement }) => {
   return (
-    <div className=' w-full md:w-3/4 lg:w-full md:mx-auto px-5 py-5 flex flex-1 justify-start gap-5 md:gap-7 bg-gray-700 mt-60'>
-            <img className='sm:w-[50px] h-[50px] md:w-[70px] md:h-[70px]' src={mardiguWowiek} alt='mardigu-wowiek' />
-            <div className='acc-content'>
-                <div className='acc-header'>
-                    <div className='header flex gap-3'>
-                        <p className='text-[16px] md:text-[20px] text-white font-sans'>Bossman Mardigu</p>
-                        <p className='text-[16px] md:text-[20px] text-gray-300 font-sans font-thin'>10h</p>
+    <Tilt>
+        <div className=' w-full md:w-3/4 lg:w-full md:mx-auto px-5 py-5 flex flex-1 justify-start gap-5 md:gap-7 bg-gray-700 mt-5'>
+                <img className='sm:w-[50px] h-[50px] md:w-[70px] md:h-[70px]' src={accountItems.map((accItem) => (
+                accItem.imageProfile
+            ))} alt='mardigu-wowiek' />
+                <div className='acc-content'>
+                    <div className='acc-header'>
+                        <div className='header flex gap-3'>
+                            <p className='text-[16px] md:text-[20px] text-white font-sans'>{
+                                accountItems.map((accItem) => (
+                                    accItem.name
+                                ))
+                            }</p>
+                            <p className='text-[16px] md:text-[20px] text-gray-300 font-sans font-thin'>{
+                                accountItems.map((accItem) => (
+                                    accItem.name
+                                ))
+                            }</p>
+                        </div>
+                        <p className='text-[14px] md:text-[16px] text-white font-sans font-thin'>{
+                            accountItems.map((accItem) => (
+                                accItem.name
+                            ))
+                        }</p>
                     </div>
-                    <p className='text-[14px] md:text-[16px] text-white font-sans font-thin'>@bossmanmardigu</p>
-                </div>
-                <p className='text-[14px] md:text-[16px] text-white mt-4'>
-                    Pada lelaki yang sukses pasti ada wanita baik yang mendukungnya. Pada lelaki yang gagal pasti banyak wanita cantik di belakangnya.
-                </p>
-                <div className='engagement flex flex-1 justify-between mt-5 md:mt-8'>
-                    <EngagementItem icon={reply} amount={'8K'} />
-                    <EngagementItem icon={retweet} amount={'15K'} />
-                    <EngagementItem icon={like} amount={'20K'} />
-                    <EngagementItem icon={bookMark} amount={'30K'} />
+                    <p className='text-[14px] md:text-[16px] text-white mt-4'>
+                        {
+                            accountItems.map((accItem) => (
+                                accItem.name
+                            ))
+                        }
+                    </p>
+                    <div className='engagement flex flex-1 justify-between mt-5 md:mt-8'>
+                        <EngagementItem icon={reply} amount={'8K'} />
+                        <EngagementItem icon={retweet} amount={'15K'} />
+                        <EngagementItem icon={like} amount={'20K'} />
+                        <EngagementItem icon={bookMark} amount={'30K'} />
+                    </div>
                 </div>
             </div>
-        </div>
+    </Tilt>
   )
 }
 
